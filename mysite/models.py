e8a22920f7199fb8d80b675573922a0af9e0b80c
Facundo Muruchi <facundomuruchi01@gmail.com>
2025-04-10 20:27:00 -0300

    host

--- mysite/models.py
+++ mysite/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField(upload_to='gifs/')
     url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Skill(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='skills/')
 
     def __str__(self):
         return self.title
--- mysite/views.py
+++ mysite/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
-from .models import Project
-
-def projects_view(request):
-    projects = Project.objects.all()
-    return render(request, 'mysite/projects.html', {'projects': projects})
+from .models import Project, Skill
 
 def base_view(request):
     projects = Project.objects.all()
-    return render(request, 'mysite/base.html', {'projects': projects})
+    skills = Skill.objects.all()
+    return render(request, 'mysite/base.html', {'projects': projects, 'skills': skills})
